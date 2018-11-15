f2b4327956a4d7f7f52740bfbf7ca245299ffe2f
glundy1984 <glundy1984@gmail.com>
2018-11-15 22:39:02 +0000

    fix button colors and remove help

--- config/routes.js
+++ config/routes.js
@@ -20,7 +20,6 @@ module.exports.routes = {
   'GET /faq':                { view:   'pages/faq' },
   'GET /legal/terms':        { view:   'pages/legal/terms' },
   'GET /legal/privacy':      { view:   'pages/legal/privacy' },
-  'GET /contact':            { view:   'pages/contact' },
 
   'GET /signup':             { action: 'entrance/view-signup' },
   'GET /email/confirm':      { action: 'entrance/confirm-email' },
